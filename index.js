2a8b892cd684a61ba31cfa1e5e40448fe9174a70
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2017-10-23 12:44:32 +0200

    refacotr: improve dependencies handling

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const sass = require('node-sass');
 const resolve = require('resolve');
 const importRegex = /@import[\s'"]*([^;'"]*)[;'"]/g;
 const includePaths = ['node_modules'];
+const STYLE_EXTENSIONS = ['.scss', '.sass', '.css'];
 
 function stylize(css) {
     return `
@@ -12,7 +13,7 @@ function stylize(css) {
 (function(){
     const head = document.head || document.getElementsByTagName('head')[0];
     const style = document.createElement('style');
-    style.textContent = '${css.replace(/\n/g, '')}';
+    style.textContent = '${css.replace(/'/g, '\\\'').replace(/\n/g, '')}';
     head.appendChild(style);
 })();
 `;
@@ -55,16 +56,12 @@ module.exports = function(options) {
     const filter = rollupPluginutils.createFilter(options.include || ['**/*.scss', '**/*.sass'], options.exclude);
     const importer = options.importer || nodeResolver;
     const defaults = options.options || {};
-    let resolved = [];
-    let files = [];
+    let file;
     let css;
     return {
         name: 'sass-modules',
         transform(code, id) {
             if (!filter(id)) { return null; }
-            if (resolved.indexOf(id) === -1) {
-                resolved = [];
-            }
             let match = importRegex.exec(code);
             let matches = [];
             while (match) {
@@ -72,44 +69,56 @@ module.exports = function(options) {
                 match = importRegex.exec(code);
             }
             matches = matches.map((url) => importer(url, id).file);
-            let jsCode = matches.map((url) => `import '${url}';`).join('\n');
+            let jsCode = '';
+            jsCode += matches.map((url, index) => `import STYLE_${index} from '${url}';`).join('\n');
+            let sassOptions = Object.assign({
+                file: id,
+                data: code,
+                includePaths,
+                importer: (url, prev) => importer(url, prev, options),
+            }, defaults);
+            sassOptions.omitSourceMapUrl = true;
+            sassOptions.sourceMapEmbed = false;
+            let rendered = sass.renderSync(sassOptions);
             let jsMaps;
-            if (resolved.length === 0) {
-                let sassOptions = Object.assign({
-                    file: id,
-                    data: code,
-                    includePaths,
-                    importer: (url, prev) => importer(url, prev, options),
-                }, defaults);
-                let rendered = sass.renderSync(sassOptions);
-                css = rendered.css.toString();
-                if (options.insert) {
-                    jsCode += stylize(css.replace(/'/g, '\\\''));
-                } else if (!defaults.outFile) {
-                    jsCode += `\nexport default '${css.replace(/'/g, '\\\'')}'`;
-                } else {
-                    files.push(css);
-                }
-                if (rendered.map) {
-                    jsMaps = rendered.map.toString();
-                }
+            css = rendered.css.toString();
+            if (rendered.map) {
+                jsMaps = rendered.map.toString();
             }
-            matches.forEach((url) => {
-                if (resolved.indexOf(url) === -1) {
-                    resolved.push(url);
-                }
-            });
-            if (jsMaps) {
-                return {
-                    code: jsCode,
-                    map: jsMaps,
-                };
+            if (options.insert) {
+                jsCode += stylize(css);
+            } else {
+                jsCode += `export default \`${css}\``;
             }
-            return jsCode;
+            return {
+                code: jsCode,
+                map: jsMaps ? jsMaps : { mappings: '' },
+            };
+        },
+        ongenerate(options) {
+            if (defaults.outFile) {
+                file = '';
+                let bundle = options.bundle;
+                bundle.modules.forEach((mod) => {
+                    if (STYLE_EXTENSIONS.indexOf(path.extname(mod.id)) === -1) {
+                        mod.dependencies.forEach((dep) => {
+                            if (STYLE_EXTENSIONS.indexOf(path.extname(dep)) !== -1) {
+                                let sassOptions = Object.assign({
+                                    file: dep,
+                                    data: fs.readFileSync(dep, 'utf8'),
+                                    includePaths,
+                                    importer: (url, prev) => importer(url, prev, options),
+                                }, defaults);
+                                file += sass.renderSync(sassOptions).css.toString();
+                            }
+                        });
+                    }
+                });
+            }
         },
         onwrite() {
-            if (defaults.outFile) {
-                fs.writeFileSync(defaults.outFile, files.join('\n'));
+            if (defaults.outFile && file) {
+                fs.writeFileSync(defaults.outFile, file);
                 return global.Promise.resolve();
             }
         },
